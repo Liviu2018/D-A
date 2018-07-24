906ecde879bb2f28b47f5eec497051e932dec862
liviuadrian.esanu@metronom.com <liviuadrian.esanu@metronom.com>
2018-07-24 20:31:51 +0200

    added test

---
+++ once/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func main() {
+	fmt.Println("main methon")
+}
+
+func connectToService() interface{} {
+	time.Sleep(1 * time.Second)
+	return struct{}{}
+}
+
+func StartNetworkDaemon() *sync.WaitGroup {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		server, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			log.Fatalf("cannot listen: %v", err)
+		}
+		defer server.Close()
+
+		wg.Done()
+
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				log.Printf("cannot accept connection: %v", err)
+				continue
+			}
+
+			connectToService()
+			fmt.Fprintln(conn, "")
+			conn.Close()
+		}
+	}()
+
+	return &wg
+}
